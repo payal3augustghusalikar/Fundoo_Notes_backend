04133400ffb52dac3b9f9c6f856fb4e914e434a1
payalghusalikar <payal.ghusalikar9@gmail.com>
2021-01-29 13:35:07 +0530

    delete : delet all crud operations and files for notes

--- app/routes/note.rt.js
+++ app/routes/note.rt.js
@@ -1,22 +1,6 @@
 module.exports = (app) => {
 
-    const notes = require('../controllers/note.ctr.js');
     const user = require('../controllers/user.ctr.js');
-
-    // Create a new note
-    app.post('/notes', notes.create);
-
-    // Retrieve all notes
-    app.get('/notes', notes.findAll);
-
-    // Retrieve a single note with noteId
-    app.get('/notes/:noteId', notes.findOne);
-
-    // Update a note with noteId
-    app.put('/notes/:noteId', notes.update);
-
-    // Delete a note with noteId
-    app.delete('/notes/:noteId', notes.delete);
 
     // register a new user
     app.post('/newRegister', user.register);
