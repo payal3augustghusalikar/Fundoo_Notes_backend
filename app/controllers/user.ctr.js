e24bf2c9ec807387881c5e956f2e15e176085bca
payalghusalikar <payal.ghusalikar9@gmail.com>
2021-02-01 18:07:33 +0530

    feat : add ternary oprator and removed if else condition for login and register

--- app/controllers/user.ctr.js
+++ app/controllers/user.ctr.js
@@ -13,7 +13,7 @@ const userService = require("../services/user.svc.js");
 const logger = require("../../logger/logger.js");
 const bcrypt = require("bcrypt");
 var helper = require("../../middleware/helper.js");
-nodemailer = require('nodemailer');
+nodemailer = require("nodemailer");
 var vallidator = require("../../middleware/vallidation.js");
 
 class userController {
@@ -37,34 +37,45 @@ class userController {
                     name: req.body.name,
                     emailId: req.body.emailId,
                     password: password,
-                    // confirmPassword: confirmPassword
                 };
-
-                const validation = vallidator.validate(userInfo)
-                if (validation.error) {
-                    return res.status(400).send({
+                const validation = vallidator.validate(userInfo);
+                return validation.error ?
+                    res.status(400).send({
                         success: false,
-                        message: validation.error.message
-                    });
-                }
+                        message: validation.error.message,
+                    }) :
+                    userService.register(userInfo, (error, data) => {
+                        //   if (error) {
+                        //     logger.error(
+                        //       "Some error occurred while creating new user, " +
+                        //         req.body.name
+                        //         (error) ?
 
-                userService.register(userInfo, (error, data) => {
-                    if (error) {
-                        logger.error(
-                            "Some error occurred while creating new user, " + req.body.name
-                        );
-                        return res.status(500).send({
-                            success: false,
-                            message: error.message
-                        });
-                    }
-                    logger.info("user added successfully !");
-                    res.status(200).send({
-                        success: true,
-                        message: "user added successfully !",
-                        data: data,
+                        //     );
+                        //     return res.status(500).send({
+                        //       success: false,
+                        //       message: error.message,
+                        //     });
+                        //   }
+
+                        return error ?
+                            res.status(500).send({
+                                success: false,
+                                message: error.message,
+                            }) :
+                            res.status(200).send({
+                                success: true,
+                                message: "user added successfully !",
+                                data: data,
+                            });
+
+                        //   logger.info("user added successfully !");
+                        //   res.status(200).send({
+                        //     success: true,
+                        //     message: "user added successfully !",
+                        //     data: data,
+                        //   });
                     });
-                });
             }
         } catch (error) {
             logger.error("Some error occurred while creating user");
@@ -95,6 +106,17 @@ class userController {
                     emailId: req.body.emailId,
                     password: password,
                 };
+
+                // password !== confirmPassword ?
+                //     res.status(400).send({
+                //         success: false,
+                //         message: "Password not match",
+                //     }) :
+                //     (userLoginInfo = {
+                //         emailId: req.body.emailId,
+                //         password: password,
+                //     });
+
                 userService.login(userLoginInfo, (error, data) => {
                     if (data.length < 1) {
                         logger.info("user not exist with emailid" + req.body.emailId);
@@ -104,7 +126,9 @@ class userController {
                             message: "Auth Failed",
                         });
                     } else {
-                        bcrypt.compare(req.body.password, data[0].password,
+                        bcrypt.compare(
+                            req.body.password,
+                            data[0].password,
                             function(err, result) {
                                 if (err) {
                                     res.status(404).send({
@@ -133,52 +157,105 @@ class userController {
         }
     };
 
+    //     try {
+    //         var confirmPassword = req.body.confirmPassword;
+    //         var password = req.body.password;
+
+    //         if (password !== confirmPassword) {
+    //             return res.status(400).send({
+    //                 success: false,
+    //                 message: "Password not match",
+    //             });
+    //         } else {
+    //             const userLoginInfo = {
+    //                 emailId: req.body.emailId,
+    //                 password: password,
+    //             };
+    //             userService.login(userLoginInfo, (error, data) => {
+    //                 if (data.length < 1) {
+    //                     logger.info("user not exist with emailid" + req.body.emailId);
+    //                     return res.status(404).send({
+    //                         success: false,
+    //                         status_code: 404,
+    //                         message: "Auth Failed",
+    //                     });
+    //                 } else {
+    //                     bcrypt.compare(
+    //                         req.body.password,
+    //                         data.password,
+    //                         function(err, result) {
+    //                             if (err) {
+    //                                 res.status(404).send({
+    //                                     success: false,
+    //                                     message: "auth Failed",
+    //                                 });
+    //                             }
+    //                         }
+    //                     );
+    //                 }
+    //                 var token = helper.createToken(data);
+    //                 return res.status(200).send({
+    //                     success: true,
+    //                     message: "login successfull",
+    //                     token: token,
+    //                 });
+    //             });
+    //         }
+    //     } catch (error) {
+    //         logger.error("could not found user with emailid" + req.body.emailId);
+    //         return res.send({
+    //             success: false,
+    //             status_code: 500,
+    //             message: "error retrieving user with emailid " + req.body.emailId,
+    //         });
+    //     }
+    // };
+
     forgotPassword = (req, res) => {
         try {
             const userInfo = {
-                emailId: req.body.emailId
-            }
-            const validationResult = emailIdPattern.validate(userInfo.emailId)
+                emailId: req.body.emailId,
+            };
+            const validationResult = emailIdPattern.validate(userInfo.emailId);
 
             if (validationResult.error) {
-                logger.error(error.message)
+                logger.error(error.message);
                 return res.status(400).send({
                     success: false,
-                    message: "please Enter correct email id"
+                    message: "please Enter correct email id",
                 });
             }
             userService.forgotPassword(userInfo, (error, user) => {
                 if (error) {
-                    logger.error(error.message)
+                    logger.error(error.message);
                     return res.status(500).send({
                         success: false,
-                        message: "error occured"
+                        message: "error occured",
                     });
                 } else if (!user) {
-                    logger.error("Authorization failed")
+                    logger.error("Authorization failed");
                     return res.status(401).send({
                         success: false,
-                        message: "Authorization failed"
+                        message: "Authorization failed",
                     });
                 } else {
-                    logger.info("Email has been sent !")
+                    logger.info("Email has been sent !");
 
                     return res.status(200).send({
                         success: true,
-                        message: "Email has been sent !"
+                        message: "Email has been sent !",
                     });
                 }
-            })
+            });
         } catch (error) {
-            logger.error("Some error occurred !")
+            logger.error("Some error occurred !");
 
             return res.status(500).send({
                 success: false,
-                message: "Authorization failed"
+                message: "Authorization failed",
             });
-
         }
-    }
+    };
 }
 
 module.exports = new userController();
