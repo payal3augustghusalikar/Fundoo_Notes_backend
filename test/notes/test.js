66c3e024e4b73bc104d8cc2b3f82e070c8c03a06
payalghusalikar <payal.ghusalikar9@gmail.com>
2021-02-12 23:26:34 +0530

    refactor : test cases for user and notes

--- test/notes/test.js
+++ test/notes/test.js
@@ -11,136 +11,137 @@ let chaiHttp = require("chai-http");
 let server = require("../../server");
 chai.use(chaiHttp);
 const greet = require("./notes.json");
+chai.should();
+//var should = require("chai").should();
 
 describe("notes API", () => {
-  /**
-   * @description Test the GET API
-   */
-  describe("GET /notes", () => {
-    // test the GET API when points are proper
-    it("givennotes_WhenGivenProperEndPoints_ShouldReturnObject", (done) => {
-      console.log("getting all data .");
-      chai
-        .request(server)
-        .get("/notes")
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a("object");
+    /**
+     * @description Test the GET API
+     */
+    describe("GET /notes", () => {
+        // test the GET API when points are proper
+        it("givennotes_WhenGivenProperEndPoints_ShouldReturnObject", (done) => {
+            console.log("getting all data .");
+            chai
+                .request(server)
+                .get("/notes")
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a("array");
+                    done();
+                });
         });
-      done();
+
+        // test the GET API when points are not proper
+        it("givennotes_WhenNotGivenProperEndPoints_ShouldNotReturnObject", (done) => {
+            chai
+                .request(server)
+                .get("/not")
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    done();
+                });
+        });
+        // done();
     });
 
-    // test the GET API when points are not proper
-    it("givennotes_WhenNotGivenProperEndPoints_ShouldNotReturnObject", (done) => {
-      chai
-        .request(server)
-        .get("/note")
-        .end((err, res) => {
-          res.should.have.status(404);
+    /**
+     * @description Test the GET API using Id
+     */
+    describe("/GET /notes/noteId", () => {
+        it("givennotes_WhenGivenProperNoteId_ShouldGiveObject", (done) => {
+            const noteId = 1;
+            chai
+                .request(server)
+                .get("/notes/" + noteId)
+                .end((err, response) => {
+                    response.should.have.status(200);
+                    response.body.should.be.a("object");
+                    done();
+                });
         });
-      done();
-    });
-  });
 
-  /**
-   * @description Test the GET API using Id
-   */
-  describe("/GET /notes/noteId", () => {
-    // test the GET API when provided proper note Id
-    it("givennotes_WhenGivenProperNoteId_ShouldGiveObject", () => {
-      //const noteId = greet.notes.GetNoteById.noteId;
-      const noteId = 9;
-      chai
-        .request(server)
-        .get("/notes/" + noteId)
-        .end((err, response) => {
-          response.should.have.status(200);
-          response.body.should.be.a("object");
+        // test the GET API when provided improper note Id
+        it("givennotes_WhenNotGivenProperNoteId_ShouldNotGiveObject", (done) => {
+            const noteId = 144;
+            chai
+                .request(server)
+                .get("/notes/" + noteId)
+                .end((err, res) => {
+                    res.should.have.status(401);
+                    // res.text.should.be.eq("task with provided id does not exist");
+                    done();
+                });
+            //
         });
-      //done();
-    });
-
-    // test the GET API when provided improper note Id
-    it("givennotes_WhenNotGivenProperNoteId_ShouldNotGiveObject", () => {
-      const noteId = 144;
-      chai
-        .request(server)
-        .get("/notes/" + noteId)
-        .end((err, res) => {
-          res.should.have.status(400);
-          res.text.should.be.eq("task with provided id does not exist");
-        });
-      // done();
-    });
-  });
-
-  /**
-   * @description Test the POST API
-   */
-  describe("POST /notes", () => {
-    // test the POST API when provided proper data
-    it("givennotes_WhenGivenPropertitleAnddescription_ShouldPostNote", () => {
-      const note = greet.notes.noteToPost;
-      chai
-        .request(server)
-        .post("/notes")
-        .send(note)
-        .end((err, response) => {
-          response.should.have.status(201);
-          response.body.should.be.a("object");
-        });
-      //done();
     });
 
-    // test the POST API when provided improper data
-    it("givennotes_WhenNotGivenPropertitleAndDescription_ShouldNotPostNote", (done) => {
-      const note = greet.notes.noteWithouttitle;
-      console.log("not post", note);
-      chai
-        .request(server)
-        .post("/notes/")
-        .send(note)
-        .end((err, res) => {
-          res.should.have.status(400);
-          res.text.should.be.eq("it is not accepting without title property");
+    /**
+     * @description Test the POST API
+     */
+    describe("POST /notes", () => {
+        // test the POST API when provided proper data
+        it("givennotes_WhenGivenPropertitleAnddescription_ShouldPostNote", (done) => {
+            const note = greet.notes.noteToPost;
+            chai
+                .request(server)
+                .post("/notes/")
+                .send(note)
+                .end((err, response) => {
+                    response.should.have.status(200);
+                    response.body.should.be.a("object");
+                    done();
+                });
         });
-      done();
-    });
-  });
 
-  /**
-   * @description Test the PUT API using Id
-   */
-  describe("/PUT  /notes/:noteId", (done) => {
-    // test the PUT API when provided proper Id
-    it("givennotes_WhenGivenProperId_ShouldUpdateNote", () => {
-      const noteId = greet.notes.noteToUpdate.noteId;
-      const note = greet.notes.noteToUpdate;
-      chai
-        .request(server)
-        .put("/notes/" + noteId)
-        .send(note)
-        .end((err, res) => {
-          res.should.have.status(200);
-          console.log("Response Body:", res.body);
-          res.body.should.be.a("Object");
+        // test the POST API when provided improper data
+        it("givennotes_WhenNotGivenPropertitleAndDescription_ShouldNotPostNote", (done) => {
+            const note = greet.notes.noteWithouttitle;
+            console.log("not post", note);
+            chai
+                .request(server)
+                .post("/notes/")
+                .send(note)
+                .end((err, res) => {
+                    res.should.have.status(401);
+                    // res.text.should.be.eq("it is not accepting without title property");
+                    done();
+                });
         });
-      done();
     });
-    // test the PUT API when provided improper Id
-    it("givennotes_WhenNotGivenPropertitle_ShouldNotUpdateNote", () => {
-      const noteId = greet.notes.noteWithouttitle.noteId;
-      const note = greet.notes.noteWithouttitle;
-      chai
-        .request(server)
-        .put("/notes/" + noteId)
-        .send(note)
-        .end((err, res) => {
-          res.should.have.status(400);
-          res.text.should.be.eq("it is not accepting without title property");
+
+    /**
+     * @description Test the PUT API using Id
+     */
+    describe("/PUT  /notes/:noteId", () => {
+        // test the PUT API when provided proper Id
+        it("givennotes_WhenGivenProperId_ShouldUpdateNote", (done) => {
+            const noteId = greet.notes.noteToUpdate.noteId;
+            const note = greet.notes.noteToUpdate;
+            chai
+                .request(server)
+                .put("/notes/" + noteId)
+                .send(note)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    console.log("Response Body:", res.body);
+                    res.body.should.be.a("Object");
+                    done();
+                });
         });
-      //done();
+        // test the PUT API when provided improper Id
+        it("givennotes_WhenNotGivenImropertitle_ShouldNotUpdateNote", (done) => {
+            const noteId = greet.notes.noteWithouttitle.noteId;
+            const note = greet.notes.noteWithouttitle;
+            chai
+                .request(server)
+                .put("/notes/" + noteId)
+                .send(note)
+                .end((err, res) => {
+                    res.should.have.status(401);
+                    // res.text.should.be.eq("it is not accepting without title property");
+                    done();
+                });
+        });
     });
-  });
-  //done();
 });
--- test/user/test.js
+++ test/user/test.js
@@ -8,152 +8,207 @@
 
 let chai = require("chai");
 let chaiHttp = require("chai-http");
+const { strict } = require("../../middleware/vallidation");
 let server = require("../../server");
 chai.use(chaiHttp);
 const userData = require("./user.json");
-let should = chai.should();
-//"test": "mocha test/user/test.js",
+chai.should();
 
 describe("register", () => {
-  it("givenUser_whenGivenProperData_shouldSaveUser", (done) => {
-    let userInfo = userData.user.registerUserProperData;
-    console.log(userInfo);
-    chai
-      .request(server)
-      .post("/register")
-      .send(userInfo)
-      .end((err, res) => {
-        res.body.status.should.have.equal(400);
-        res.body.should.be.a("object");
-      });
-    done();
-  });
+    it("givenUser_whenGivenProperData_shouldSaveUser", (done) => {
+        let userInfo = userData.user.registerUserProperData;
+        console.log("userInfo: " + userInfo);
+        chai
+            .request(server)
+            .post("/register")
+            .send(userInfo)
+            .end((err, res) => {
+                console.log("body : " + res.body);
+                res.should.have.status(200);
+                res.body.should.be.a("object");
+                console.log("object : " + object);
+                done();
+            });
+    });
 
-  it("givenUser_whenGivenDuplicateData_shouldNotSaveUser", () => {
-    let userInfo = userData.user.registerUserProperData;
-    chai
-      .request(server)
-      .post("/register")
-      .send(userInfo)
+    it("givenUser_whenGivenDuplicateData_shouldNotSaveUser", (done) => {
+        let userInfo = userData.user.registerUserProperData;
+        chai
+            .request(server)
+            .post("/register")
+            .send(userInfo)
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.body.should.be.a("object");
+                done();
+            });
+    });
+    it("givenUser_whenGivenImproperData_shouldNotSaveUser", (done) => {
+        let userInfo = userData.user.userWithEmptyName;
+        chai
+            .request(server)
+            .post("/register")
+            .send(userInfo)
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.body.should.be.a("object");
 
-      .end((err, res) => {
-        res.body.status.should.have.equal(400);
-        res.body.should.be.a("object");
-      });
-    //done();
-  });
-  it("givenUser_whenGivenImproperData_shouldNotSaveUser", () => {
-    let userInfo = userData.user.userWithEmptyName;
-    chai
-      .request(server)
-      .post("/register")
-      .send(userInfo)
-      .end((err, res) => {
-        res.should.have.status(400);
-        res.body.should.be.a("object");
-      });
-    done();
-  });
+                done();
+            });
+    });
+    it("givenUser_whenGivenImproperDataEmail_shouldNotSaveUser", (done) => {
+        let userInfo = userData.user.userWithImproperEmail;
+        chai
+            .request(server)
+            .post("/register")
+            .send(userInfo)
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.body.should.be.a("object");
+
+                done();
+            });
+    });
+    it("givenUser_whenGivenEmptyEmail_shouldNotSaveUser", (done) => {
+        let userInfo = userData.user.userWithEmptyEmail;
+        chai
+            .request(server)
+            .post("/register")
+            .send(userInfo)
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.body.should.be.a("object");
+
+                done();
+            });
+    });
 });
 
 describe("Login", () => {
-  it("givenUser_whenGivenProperData_shouldRespondsWithJson", (done) => {
-    let userInfo = userData.user.loginUserProperData;
-    chai
-      .request(server)
-      .post("/login")
-      .send(userInfo)
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a("object");
-        done();
-      });
-  });
+    it("givenUser_whenGivenProperData_shouldRespondsWithJson", (done) => {
+        let userInfo = userData.user.loginUserProperData;
+        chai
+            .request(server)
+            .post("/login")
+            .send(userInfo)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a("object");
+                done();
+                // console.log("done: " + done());
+            });
+        // console.log("done: " + done);
+    });
 
-  it("givenUser_whenGivenImproperData_shouldRespondsWithJson", (done) => {
-    let userInfo = userData.user.loginUserImproperData;
-    chai
-      .request(server)
-      .post("/login")
-      .send(userInfo)
-      .end((err, res) => {
-        res.should.have.status(401);
-        res.body.should.be.a("object");
-        done();
-      });
-  });
+    it("givenUser_whenGivenEmptyEmail_shouldNotLoginUser", (done) => {
+        let userInfo = userData.user.loginUserEmptyEmail;
+        chai
+            .request(server)
+            .post("/login")
+            .send(userInfo)
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.body.should.be.a("object");
+                done();
+            });
+    });
+    it("givenUser_whenGivenImproperData_shouldNotRespondsWithJson", (done) => {
+        let userInfo = userData.user.loginUserImproperData;
+        chai
+            .request(server)
+            .post("/login")
+            .send(userInfo)
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.body.should.be.a("object");
+                done();
+            });
+    });
 });
 
 describe("ForgotPassword", () => {
-  it("givenUser_whenGivenImproperData_shouldRespondsWithLink", (done) => {
-    let userInfo = userData.user.forgotPasswordProperData;
-    chai
-      .request(server)
-      .post("/forgotpassword")
-      .send(userInfo)
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a("object");
-      });
-    done();
-  });
+    it("givenUser_whenGivenProperData_shouldRespondsWithLink", (done) => {
+        let userInfo = userData.user.forgotPasswordProperData;
+        chai
+            .request(server)
+            .post("/forgotpassword")
+            .send(userInfo)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a("object");
+                done();
+            })
+            .catch(done);
+    });
 
-  it("givenUser_whenGivenImproperData_shouldNotRespondsWithLink", (done) => {
-    let userInfo = userData.user.forgotPasswordImproperData;
-    chai
-      .request(server)
-      .post("/forgotpassword")
-      .send(userInfo)
-      .end((err, res) => {
-        res.should.have.status(401);
-        res.body.should.be.a("object");
-      });
-    done();
-  });
+    it("givenUser_whenGivenImproperData_shouldNotRespondsWithLink", (done) => {
+        let userInfo = userData.user.forgotPasswordImproperData;
+        chai
+            .request(server)
+            .post("/forgotpassword")
+            .send(userInfo)
+            .end((err, res) => {
+                res.should.have.status(500);
+                res.body.should.be.a("object");
+                done();
+            });
+    });
+    it("givenUser_whenGivenImproperEmail_shouldNotRespondsWithLink", (done) => {
+        let userInfo = userData.user.loginUserImproperEmail;
+        chai
+            .request(server)
+            .post("/forgotpassword")
+            .send(userInfo)
+            .end((err, res) => {
+                res.should.have.status(500);
+                res.body.should.be.a("object");
+                done();
+            });
+    });
 });
 
 describe("Resetpassword", () => {
-  it("givenUser_whenGivenProperData_shouldResetPassword", (done) => {
-    let userInfo = userData.user.resetPasswordProperData;
-    let token = userData.user.properToken;
-    chai
-      .request(server)
-      .put("/resetpassword")
-      .send(userInfo)
-      .set("token", token)
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a("object");
-      });
-    done();
-  });
-  it("givenUser_whenGivenImproperData_shouldNotResetPassword", (done) => {
-    let userInfo = userData.user.resetPasswordImproperData;
-    let token = userData.user.properToken;
-    chai
-      .request(server)
-      .put("/resetpassword")
-      .send(userInfo)
-      .set("token", token)
-      .end((err, res) => {
-        res.should.have.status(400);
-        res.body.should.be.a("object");
-      });
-    done();
-  });
+    it("givenUser_whenGivenProperData_shouldResetPassword", (done) => {
+        let userInfo = userData.user.resetPasswordProperData;
+        let token = userData.user.properToken;
+        chai
+            .request(server)
+            .put("/resetpassword")
+            .send(userInfo)
+            .set("token", token)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a("object");
+                done();
+            });
+    });
+    it("givenUser_whenGivenImproperData_shouldNotResetPassword", (done) => {
+        let userInfo = userData.user.resetPasswordImproperData;
+        let token = userData.user.properToken;
+        chai
+            .request(server)
+            .put("/resetpassword")
+            .send(userInfo)
+            .set("token", token)
+            .end((err, res) => {
+                res.should.have.status(401);
+                res.body.should.be.a("object");
+                done();
+            });
+    });
 
-  it("givenUser_whenGivenImproperToken_shouldNotResetPassword", (done) => {
-    let userInfo = userData.user.resetPasswordProperData;
-    let token = userData.user.ImproperToken;
-    chai
-      .request(server)
-      .put("/resetpassword")
-      .send(userInfo)
-      .set("token", token)
-      .end((err, res) => {
-        res.should.have.status(400);
-        res.body.should.be.a("object");
-      });
-    done();
-  });
+    it("givenUser_whenGivenImproperToken_shouldNotResetPassword", (done) => {
+        let userInfo = userData.user.resetPasswordProperData;
+        let token = userData.user.ImproperToken;
+        chai
+            .request(server)
+            .put("/resetpassword")
+            .send(userInfo)
+            .set("token", token)
+            .end((err, res) => {
+                res.should.have.status(401);
+                res.body.should.be.a("object");
+                done();
+            });
+    });
 });
