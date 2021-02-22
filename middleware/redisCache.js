25e2b25eb8e8d1bd9d83344cde752015b8eb9195
payalghusalikar <payal.ghusalikar9@gmail.com>
2021-02-23 00:20:58 +0530

    delete : unwanted code

--- middleware/redisCache.js
+++ middleware/redisCache.js
@@ -13,63 +13,12 @@ client.on("error", function(err) {
     console.log("Redis error: " + err);
 });
 
-/**
- * @description this method is to check redis data is present in cache or not
- * @param {*} userEmail for unique identity
- * @param {*} res to send responce with redis data if data is present
- * @param {*} next will call next function if data is not present
- */
-// checkCacheData = (userEmail, res, next) => {
-//     return client.get(
-//         `process.env.REDIS_KEY ${userEmail}`,
-//         (error, redisData) => {
-//             console.log("start : ", redisData);
-//             return error || redisData == null ?
-//                 (console.log("no data in redis"),
-//                     logger.error("Error retrieving data from redis cache"),
-//                     next()) :
-//                 (console.log("redisDataa : " + JSON.parse(redisData)),
-//                     res.send({
-//                         status_code: status.Success,
-//                         message: `data found`,
-//                         redisData: JSON.parse(redisData),
-//                     }));
-//         }
-//     );
-// };
-
 class RedisCache {
     /**
-     * @description this methods calls from routes to check redis data
+     * @description this methods calls from routes to check redis data for label
      * @param {*} req is to take email from body
      * @param {*} res to send response
      * @param {*} next is pass as a argument
-     */
-    // redisGet = async(req, res, next) => {
-    //     var start = new Date();
-    //     const userEmail = req.body.emailId;
-    //     console.log("get email from body :", userEmail);
-    //     if (userEmail == undefined) {
-    //         const token = req.headers.authorization.split(" ")[1];
-    //         const userEmail = helper.getEmailFromToken(token);
-    //         console.log("get email from token:", userEmail);
-    //         console.log("inside note or label");
-    //         const data = await checkCacheData(userEmail, res, next);
-    //         console.log("return from check cache: ", data);
-    //         console.log("Request took2:", new Date() - start, "ms");
-    //     } else {
-    //         console.log("for login");
-    //         const data = await checkCacheData(userEmail, res, next);
-    //         console.log("return from check cache: ", data);
-    //         console.log("Request took1:", new Date() - start, "ms");
-    //     }
-    // };
-
-    //class RedisCache {
-    /**
-     * @param {*} req
-     * @param {*} res
-     * @param {*} next
      */
     redisGetLabel = (req, res, next) => {
         console.log("");
@@ -97,6 +46,12 @@ class RedisCache {
         );
     };
 
+    /**
+     * @description this methods calls from routes to check redis data for notes
+     * @param {*} req is to take email from body
+     * @param {*} res to send response
+     * @param {*} next is pass as a argument
+     */
     redisGetNotes = (req, res, next) => {
         console.log("");
         console.log("redisGetNotes");
@@ -123,6 +78,12 @@ class RedisCache {
         );
     };
 
+    /**
+     * @description this methods calls from routes to check redis data for Login
+     * @param {*} req is to take email from body
+     * @param {*} res to send response
+     * @param {*} next is pass as a argument
+     */
     redisGetLogin = (req, res, next) => {
         console.log("");
         console.log("redisGetLogin");
