e15cad851921e2a3016790fd3f7400d6dd4bb660
payalghusalikar <payal.ghusalikar9@gmail.com>
2021-02-26 06:40:31 +0530

    wip : rabbitmq implementation in progress

--- middleware/publisher.js
+++ middleware/publisher.js
@@ -3,19 +3,19 @@ const amqp = require("amqplib/callback_api");
 amqp.connect("amqp://localhost", (error, connection) => {
     if (error) {
         //  logger.connect("Error while connecting to Rabbit Mq");
-        callBack(error, null);
+        callback(error, null);
     }
     connection.createChannel((error, channel) => {
         if (error) {
             logger.error("Error while creating chnannel");
-            callBack(error, null);
+            callback(error, null);
         }
-        let queName = "EmailIdInQueue";
+        let queueName = "EmailIdInQueue";
         let message = "this is just for message";
         channel.assertQueue(queueName, {
             durable: false,
         });
-        channel.sendToQueue(queName, Buffer.from(message));
+        channel.sendToQueue(queueName, Buffer.from(message));
         console.log(`Message : ${message}`);
         setTimeout(() => {
             connection.close();
--- middleware/subscriber.js
+++ middleware/subscriber.js
@@ -3,21 +3,21 @@ const amqp = require("amqplib/callback_api");
 amqp.connect(someUrl, (error, connection) => {
     if (error) {
         logger.connect("Error while connecting to Rabbit Mq");
-        return callBack(error, null);
+        return callback(error, null);
     }
     connection.createChannel((error, channel) => {
         if (error) {
             logger.error("Error while creating chnannel");
-            return callBack(error, null);
+            return callback(error, null);
         }
-        let queName = "EmailIdInQueue";
-        channel.assertQueue(queName, {
+        let queueName = "EmailIdInQueue";
+        channel.assertQueue(queueName, {
             durable: false,
         });
         channel.consume(queName, (msg) => {
             console.log(`Message: ${msg.content.toString()}`);
             channel.ack(msg);
-            return callBack(null, msg.content.toString());
+            return callback(null, msg.content.toString());
         });
     });
 });
